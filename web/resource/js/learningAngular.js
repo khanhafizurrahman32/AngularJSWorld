9ba841829af9bf46126867d6bbc60869ab511d78
khanhafizurrahman32 <khr.1380@gmail.com>
2017-05-15 15:42:30 +0200

    more than one watch variable add and handle it using object mechanism

--- web/resource/js/learningAngular.js
+++ web/resource/js/learningAngular.js
@@ -1,41 +1,52 @@
 var myModule = angular.module('myModule', []);
 
-myModule.factory('mySharedService', function($rootScope) {
+myModule.factory('mySharedService', ['$rootScope',function($rootScope) {
     var sharedService = {};
-    
+
     sharedService.message = '';
+    sharedService.name = '';
 
     sharedService.prepForBroadcast = function(msg) {
         this.message = msg;
         this.broadcastItem();
     };
 
+    sharedService.nameBroadcast = function(name) {
+        this.name = name;
+        this.broadcastItem();
+    };
     sharedService.broadcastItem = function() {
         $rootScope.$broadcast('handleBroadcast');
     };
 
     return sharedService;
-});
+}]);
 
-myModule.controller('ControllerZero',['$scope', 'mySharedService',function ($scope, mySharedService1) {
+myModule.controller('ControllerZero',['$scope', 'mySharedService',function ($scope, mySharedService) {
     $scope.message = 'Hello';
+    $scope.name = 'World';
+
     $scope.$watch('message',function (msg) {
-        mySharedService1.prepForBroadcast(msg);
+        mySharedService.prepForBroadcast(msg);
+    });
+
+    $scope.$watch('name',function (name) {
+        mySharedService.nameBroadcast(name);
     });
 
     $scope.$on ('handleBroadcast', function () {
-        $scope.message = mySharedService1.message;
+        $scope.message = mySharedService.message;
     });
 }])
 
 myModule.controller('ControllerOne',['$scope', 'mySharedService', function ($scope, mySharedService) {
     $scope.$on('handleBroadcast', function() {
-        $scope.message = 'ONE: ' + mySharedService.message;
+        $scope.message = 'ONE: ' + mySharedService.message + ' ' + mySharedService.name;
     });
 }])
 
 myModule.controller('ControllerTwo',['$scope', 'mySharedService', function ($scope, mySharedService) {
     $scope.$on('handleBroadcast', function() {
-        $scope.message = 'TWO: ' + mySharedService.message;
+        $scope.message = 'TWO: ' + mySharedService.message + ' ' + mySharedService.name;
     });
 }])
